cfe320172f07f95d0815d2654ee4643bd5facbfd
geovannioytamay <argentina.geovanni@gmail.com>
2023-03-06 15:05:35 -0600

    regresar 2

--- src/keys.js
+++ src/keys.js
@@ -3,11 +3,15 @@ module.exports = {
     database: {
         connectionLimit: 10,
         timeout  : 100000,         
-        host: DB_HOST=process.env.DB_HOST || 'localhost',
-        user: DB_USER=process.env.DB_USER || 'root',
-        password: DB_PASSWORD=process.env.DB_PASSWORD || 'root',
-        port: DB_PORT=process.env.DB_PORT || 3306,
-        database: DB_NAME=process.env.DB_NAME || 'basepelis'
+        host: 'containers-us-west-159.railway.app',
+        user: 'root',
+        password: 'TAtBV7lxQza08G9U3fjP',
+        port: 6612,
+        database: 'railway'        
+
+       
+
     }
+  
 
 };
