90952e1f05f35649d95f36d4892b2c8d0384bc24
geovannioytamay <argentina.geovanni@gmail.com>
2023-03-13 17:45:35 -0600

    CREDENCIAKES

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 var config={
     twitter:{
-        key:'23424234231jhkjhkj',
-        secret:'kjcnkajnacjdrnrm,ere'
+        key:KEY_TWITTER=process.env.KEY_TWITTER || '23424234231jhkjhkj',
+        secret: SECRET_TWITTER=process.env.SECRET_TWITTER || 'kjcnkajnacjdrnrm,ere'
     },
     facebook:{
-        key:'769266484381034',
-        secret:'5e4d0a16360f9990a0cd039d4bd733b2'
+        key:KEY_FACEBOOK=process.env.KEY_FACEBOOK || '769266484381034',
+        secret:SECRET_FACEBOOK=process.env.SECRET_FACEBOOK || '5e4d0a16360f9990a0cd039d4bd733b2'
     },
     google:{
-        key:'AIzaSyBOHAB7i7x1CCkdK3E4jD2bv3LAI85QMv0',
-        secret:'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'
+        key: KEY_GOOGLE=process.env.KEY_GOOLGE ||'AIzaSyBOHAB7i7x1CCkdK3E4jD2bv3LAI85QMv0',
+        secret: SECRET_GOOGLE=process.env.SECRET_GOOGLE ||'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'
     },
     instagram:{
-        key:'578796390954993',
-        secret:'a8171c90f3119df38d8e880e2acb4c9a'
+        key:KEY_INSTAGRAM=process.env.KEY_INSTAGRAM || '578796390954993',
+        secret:SECRET_INSTAGRAM=process.env.SECRET_INSTAGRAM || 'a8171c90f3119df38d8e880e2acb4c9a'
     }
 
 
