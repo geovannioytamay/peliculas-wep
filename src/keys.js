08cdbd77ba5661c1c85d1ec84b1822eea30149ac
edwin <argentina.geovanni@gmail.com>
2023-06-28 22:43:47 -0600

    ssl actualizado

--- src/keys.js
+++ src/keys.js
@@ -3,11 +3,14 @@ module.exports = {
     database: {
         connectionLimit: 10,
         timeout  : 100000,         
-        host: DB_HOST=process.env.DB_HOST || 'localhost',
-        user: DB_USER=process.env.DB_USER || 'root',
-        password: DB_PASSWORD=process.env.DB_PASSWORD || 'root',
+        host: DB_HOST=process.env.DB_HOST || 'aws.connect.psdb.cloud',
+        user: DB_USER=process.env.DB_USER || 'm1k3ycyplhkkaum41gz9',
+        password: DB_PASSWORD=process.env.DB_PASSWORD || 'pscale_pw_kFsIEJfoQyekz1VMEVs3vHWseO4xCiS8I7755lxy27q',
         port: DB_PORT=process.env.DB_PORT || 3306,
-        database: DB_NAME=process.env.DB_NAME || 'basepelis'       
+        database: DB_NAME=process.env.DB_NAME || 'basepelis' ,
+        ssl:{
+            rejetUnauthorized:false
+        }      
      
     }
   
